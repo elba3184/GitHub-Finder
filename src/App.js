523577f915f889237484d1e560220352e4bd5939
Elba Chimilio <elbachimilio@gmail.com>
2020-01-01 16:10:34 -0500

    Added clear button to Search Component

--- src/App.js
+++ src/App.js
@@ -27,15 +27,20 @@ class App extends Component {
         &client_secret=${process.env.GITHUB_CLIENT_SECRET}`
       );
       this.setState({ users: res.data.items, loading: false });
-  }
+  };
+
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
 
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers}/>
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false}/>
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
