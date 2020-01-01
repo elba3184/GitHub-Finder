32499ef8e1fadf186a79e968060fec5677df1200
Elba Chimilio <elbachimilio@gmail.com>
2020-01-01 15:38:51 -0500

    Added Search bar component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-
   state = {
     users: [],
     loading: false
   };
 
   async componentDidMount() {
-    this.setState({ loading: true }); 
-    const res = await axios.get('https://api.github.com/users');
+    console.log(process.env.GITHUB_CLIENT_SECRET);
+    this.setState({ loading: true });
+    const res = await axios.get(
+    `https://api.github.com/users?client_id=${process.env.GITHUB_CLIENT_ID}
+    &client_secret=${process.env.GITHUB_CLIENT_SECRET}`);
     this.setState({ users: res.data, loading: false });
-  };
+    console.log(this.state.users)
+  }
 
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <div className="container">
-          <Users loading={this.state.loading} users={this.state.users}/>
+        <div className='container'>
+          <Search />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
