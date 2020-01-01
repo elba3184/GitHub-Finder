6ba9c245f606bf72a3326ccb799f53b5098ab50b
Elba Chimilio <elbachimilio@gmail.com>
2020-01-01 15:54:02 -0500

    Finished implementing Search users component

--- src/App.js
+++ src/App.js
@@ -11,14 +11,22 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
-    console.log(process.env.GITHUB_CLIENT_SECRET);
+  // async componentDidMount() {
+  //   console.log(process.env.GITHUB_CLIENT_SECRET);
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //   `https://api.github.com/users?client_id=${process.env.GITHUB_CLIENT_ID}
+  //   &client_secret=${process.env.GITHUB_CLIENT_SECRET}`);
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  searchUsers = async (text) => {
     this.setState({ loading: true });
-    const res = await axios.get(
-    `https://api.github.com/users?client_id=${process.env.GITHUB_CLIENT_ID}
-    &client_secret=${process.env.GITHUB_CLIENT_SECRET}`);
-    this.setState({ users: res.data, loading: false });
-    console.log(this.state.users)
+      const res = await axios.get(
+       `https://api.github.com/search/users?q=${text}&client_id=${process.env.GITHUB_CLIENT_ID}
+        &client_secret=${process.env.GITHUB_CLIENT_SECRET}`
+      );
+      this.setState({ users: res.data.items, loading: false });
   }
 
   render() {
@@ -26,7 +34,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search />
+          <Search searchUsers={this.searchUsers}/>
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
